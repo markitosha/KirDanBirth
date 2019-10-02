ac570c94bc56e86e28bb790b6ffe3c4098fdea97
markitosha <nataly_106@mail.ru>
2019-10-03 01:08:18 +0300

    Добавлен билд для робушки

---
+++ build/precache-manifest.01ef1770a27094164daf3b45972d7d14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5868bd0f78e02e4835a63974c9ea5632",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35a0ae4d1ebe61dc256f",
+    "url": "/static/css/main.8f5fd763.chunk.css"
+  },
+  {
+    "revision": "50821a6cb9ef72f5085d",
+    "url": "/static/js/2.92afad4d.chunk.js"
+  },
+  {
+    "revision": "35a0ae4d1ebe61dc256f",
+    "url": "/static/js/main.10f1a4ec.chunk.js"
+  },
+  {
+    "revision": "a1aa6001d7c798d3a44c",
+    "url": "/static/js/runtime-main.d4aa0f35.js"
+  },
+  {
+    "revision": "b33dcae3b502fd54ac7ee94d53700ac1",
+    "url": "/static/media/DAN.b33dcae3.JPG"
+  }
+]);
