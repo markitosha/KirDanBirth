bb865ffab72c3a0315b813434f9f7b9d43324db1
markitosha <nataly_106@mail.ru>
2019-10-03 11:43:02 +0300

    Пересобрана статика

---
+++ build/precache-manifest.d6ea2b94012985339dcb6a1376ede9f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f73d94bf5d6ec690f77ec41a8f9a653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67212cbe851a89e0f86f",
+    "url": "/static/css/main.f1add33e.chunk.css"
+  },
+  {
+    "revision": "50821a6cb9ef72f5085d",
+    "url": "/static/js/2.92afad4d.chunk.js"
+  },
+  {
+    "revision": "67212cbe851a89e0f86f",
+    "url": "/static/js/main.06aaedb3.chunk.js"
+  },
+  {
+    "revision": "a1aa6001d7c798d3a44c",
+    "url": "/static/js/runtime-main.d4aa0f35.js"
+  },
+  {
+    "revision": "b33dcae3b502fd54ac7ee94d53700ac1",
+    "url": "/static/media/DAN.b33dcae3.JPG"
+  }
+]);
