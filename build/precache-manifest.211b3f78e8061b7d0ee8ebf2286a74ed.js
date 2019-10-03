cf553676da4fb3f69317808e21b2c8d191317e3d
markitosha <nataly_106@mail.ru>
2019-10-03 19:06:14 +0300

    Новый билд!

---
+++ build/precache-manifest.211b3f78e8061b7d0ee8ebf2286a74ed.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd02b7de2e160eb68d01a33fb43ec07d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45e34f2d30fe26409fd8",
+    "url": "/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "a30c0492aa21490b878f",
+    "url": "/static/css/main.e035758e.chunk.css"
+  },
+  {
+    "revision": "45e34f2d30fe26409fd8",
+    "url": "/static/js/2.c08c874c.chunk.js"
+  },
+  {
+    "revision": "a30c0492aa21490b878f",
+    "url": "/static/js/main.89eae0cb.chunk.js"
+  },
+  {
+    "revision": "a1aa6001d7c798d3a44c",
+    "url": "/static/js/runtime-main.d4aa0f35.js"
+  },
+  {
+    "revision": "d53b6976ecb58ed6daabd18e75478059",
+    "url": "/static/media/Anna.d53b6976.jpg"
+  },
+  {
+    "revision": "b33dcae3b502fd54ac7ee94d53700ac1",
+    "url": "/static/media/DAN.b33dcae3.JPG"
+  },
+  {
+    "revision": "cf7ae728ce069d36eb3b372587ce0ab8",
+    "url": "/static/media/ROB.cf7ae728.mp4"
+  },
+  {
+    "revision": "861b01f4e6520c987672775c734fd200",
+    "url": "/static/media/natula.861b01f4.JPG"
+  },
+  {
+    "revision": "692a0628ebba248bbd33c902b09d3ead",
+    "url": "/static/media/sana.692a0628.jpg"
+  }
+]);
