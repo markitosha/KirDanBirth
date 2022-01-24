0dfec1c4e5d63c2e54c7d431e25cf9243a06613c
markitosha <nataly_106@mail.ru>
2022-01-24 20:26:51 +0300

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Step from './Step';
 import FormStep from './FormStep';
 import { steps } from '../constants';
