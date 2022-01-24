cee3ed50e1a7233b8b63cae59301e6561b4dc5aa
markitosha <nataly_106@mail.ru>
2022-01-24 20:26:24 +0300

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,14 +3,12 @@ import Step from './Step';
 import FormStep from './FormStep';
 import { steps } from '../constants';
 
-let fetched = false;
-
 const App: React.FC = () => {
     const [step, setStep] = useState(0);
     const [word, setWord] = useState('');
     const [textColor, setColor] = useState('000000');
 
-    if (!fetched) {
+    useEffect(() => {
         fetch('https://kirdan.ru', {
             mode: 'cors',
             method: 'post',
@@ -18,12 +16,11 @@ const App: React.FC = () => {
             .then(async data => {
                 const res = await data.json();
 
-                fetched = true;
                 setWord(res.image_text);
                 setColor(res.image_text_color);
             })
             .catch(() => {});
-    }
+    }, []);
 
     const nextStep = () => {
         setStep(step + 1);
